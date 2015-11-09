01600893f968ce33cccd91600e354243b2743a55
Xavier Fischer <xavier@fischer.ninja>
2015-11-09 18:06:27 +0100

    LIcense and nuget preparation

--- SqlServerSpatialTypes.Toolkit/Properties/AssemblyInfo.cs
+++ SqlServerSpatialTypes.Toolkit/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("SqlServerTypes.Toolkit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SqlServerSpatialTypes.Toolkit.Core")]
+[assembly: AssemblyDescription("Sql Server spatial types trace and trace viewer.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlServerTypes.Toolkit")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Xavier Fischer")]
+[assembly: AssemblyProduct("SqlServerSpatialTypes.Toolkit")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("Xavier Fischer")]
 [assembly: AssemblyCulture("")]
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
