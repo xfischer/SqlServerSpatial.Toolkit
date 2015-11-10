2925213cc2153aeaabfecee218c45de874ba5550
Xavier Fischer <xavier_fischer@hotmail.fr>
2015-11-10 11:56:53 +0100

    Issue #4 (ninja code for Caller attributes)

---
+++ SqlServerSpatialTypes.Toolkit/Extensions/CallerAttributes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if NET45
+#else
+namespace System.Runtime.CompilerServices
+{
+	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+	public class CallerMemberNameAttribute : Attribute
+	{
+	}
+
+	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+	public class CallerFilePathAttribute : Attribute
+	{
+	}
+
+	[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+	public class CallerLineNumberAttribute : Attribute
+	{
+	}
+}
+#endif
