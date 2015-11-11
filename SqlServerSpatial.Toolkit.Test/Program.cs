ba60a7fb8532e50e1f5bf1abd30c81ac2018640b
Xavier Fischer <xavier@fischer.ninja>
2015-11-11 23:14:45 +0100

    Cleaned test program

--- SqlServerSpatial.Toolkit.Test/Program.cs
+++ SqlServerSpatial.Toolkit.Test/Program.cs
@@ -10,10 +10,17 @@ namespace SqlServerSpatial.Toolkit.Test
 {
 	class Program
 	{
+		[STAThread]
 		static void Main(string[] args)
 		{
 			SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
 
+			//TestTrace();
+			TestCentroid();
+		}
+
+		private static void TestTrace()
+		{
 			// Test with SqlGeometry
 			SqlGeometry geometry = SqlGeometry.STPolyFromText(new SqlChars(new SqlString("POLYGON((6.591796875 44.43377984606825,4.1748046875 44.402391829093915,4.21875 42.35854391749705,6.85546875 42.391008609205045,6.591796875 44.43377984606825))")), 4326);
 
@@ -28,21 +35,30 @@ namespace SqlServerSpatial.Toolkit.Test
 			SpatialTrace.TraceGeometry(geometry, "Sample geometry with default style");
 			SpatialTrace.SetLineWidth(3); // 3 pixels wide stroke
 			SpatialTrace.Indent();
-				SpatialTrace.TraceGeometry(geometryBuf, "Positive buffer");
-				SpatialTrace.TraceGeometry(geometryBuf2, "Negative buffer");
-				SpatialTrace.SetLineWidth(1); // 1 pixel wide stroke
-				SpatialTrace.SetFillColor(Color.FromArgb(128, 255, 0, 0)); // Fill with red
-				SpatialTrace.SetLineColor(Color.FromArgb(128, 0, 0, 255)); // Blue stroke
-				SpatialTrace.Indent();
-					SpatialTrace.TraceGeometry(geometry, "Sample geometry with custom style");
-					SpatialTrace.SetFillColor(Color.FromArgb(128, 0, 0, 255)); // Fill with blue
-					SpatialTrace.SetLineColor(Color.FromArgb(128, 0, 255, 0)); // Red stroke
-					SpatialTrace.TraceGeometry(geography, "Sample geography");
-				SpatialTrace.Unindent();
+			SpatialTrace.TraceGeometry(geometryBuf, "Positive buffer");
+			SpatialTrace.TraceGeometry(geometryBuf2, "Negative buffer");
+			SpatialTrace.SetLineWidth(1); // 1 pixel wide stroke
+			SpatialTrace.SetFillColor(Color.FromArgb(128, 255, 0, 0)); // Fill with red
+			SpatialTrace.SetLineColor(Color.FromArgb(128, 0, 0, 255)); // Blue stroke
+			SpatialTrace.Indent();
+			SpatialTrace.TraceGeometry(geometry, "Sample geometry with custom style");
+			SpatialTrace.SetFillColor(Color.FromArgb(128, 0, 0, 255)); // Fill with blue
+			SpatialTrace.SetLineColor(Color.FromArgb(128, 0, 255, 0)); // Red stroke
+			SpatialTrace.TraceGeometry(geography, "Sample geography");
 			SpatialTrace.Unindent();
+			SpatialTrace.Unindent();
+			SpatialTrace.ShowDialog();
+		}
 
+		static void TestCentroid()
+		{
+			SqlGeometry geometry = SqlGeometry.STPolyFromText(new SqlChars(new SqlString("POLYGON((6.591796875 44.43377984606825,4.1748046875 44.402391829093915,4.21875 42.35854391749705,6.85546875 42.391008609205045,6.591796875 44.43377984606825))")), 4326);
+			SqlGeometry centroid = geometry.STCentroid();
 
-
+			SpatialTrace.TraceGeometry(geometry, "geometry");
+			SpatialTrace.SetFillColor(Colors.Red);
+			SpatialTrace.TraceGeometry(centroid, "Centroid");
+			SpatialTrace.ShowDialog();
 		}
 	}
 }
