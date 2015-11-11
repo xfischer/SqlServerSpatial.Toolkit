4f4a3095240ae1f278382e9235997028f9ca1edf
Xavier Fischer <xavier@fischer.ninja>
2015-11-11 23:00:27 +0100

    moved nuget files to propert directory

--- SqlServerSpatial.Toolkit NuGet/NuGet-pack.bat
+++ SqlServerSpatial.Toolkit NuGet/NuGet-pack.bat
@@ -1,4 +1,4 @@
-cd D:\Repositories\SqlServerSpatial.Toolkit\SqlServerSpatial.Toolkit
+cd ..\SqlServerSpatial.Toolkit
 nuget pack SqlServerSpatial.Toolkit.core.csproj -Prop Configuration=Release
 rem "Remove any Microsoft.SqlServer.Types content directory from package file"
 pause
