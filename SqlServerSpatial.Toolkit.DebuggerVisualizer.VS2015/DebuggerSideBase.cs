32fcfec6d50e2868b247d3e87a7de30606ba6b4c
Xavier Fischer <xavier.fischer@outlook.com>
2016-10-20 10:41:51 +0200

    nuget test

---
+++ SqlServerSpatial.Toolkit.DebuggerVisualizer.VS2015/DebuggerSideBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using Microsoft.SqlServer.Types;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+using SqlServerSpatial.Toolkit.Debugging;
+
+namespace SqlServerSpatial.Toolkit.Visualizer
+{
+    public abstract class DebuggerSideBase : DialogDebuggerVisualizer
+	{
+
+		protected abstract SqlGeometry GetObject(IVisualizerObjectProvider objectProvider);
+
+		protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+		{
+
+			try
+			{
+				SqlGeometry geometry = GetObject(objectProvider);
+
+				using (FrmGeometryViewer frmViewer = new FrmGeometryViewer())
+				{
+					frmViewer.SetGeometry(SqlGeomStyledFactory.Create(geometry, null,  Color.FromArgb(200, 0, 175, 0), Colors.Black, 1f));
+
+					// Show the grid with the list
+					windowService.ShowDialog(frmViewer);
+				}
+			}
+			catch (Exception e)
+			{
+				System.Windows.MessageBox.Show("Error: " + e.ToString());
+			}
+		}
+	}
+
+	public abstract class DebuggerSideListBase : DialogDebuggerVisualizer
+	{
+
+		protected abstract IEnumerable<SqlGeometry> GetObject(IVisualizerObjectProvider objectProvider);
+
+		protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+		{
+
+			try
+			{
+				IEnumerable<SqlGeometry> geometry = GetObject(objectProvider);
+
+				using (FrmGeometryViewer frmViewer = new FrmGeometryViewer())
+				{
+					frmViewer.SetGeometry(SqlGeomStyledFactory.Create(geometry, null, Color.FromArgb(200, 0, 175, 0), Colors.Black, 1f));
+
+					// Show the grid with the list
+					windowService.ShowDialog(frmViewer);
+				}
+			}
+			catch (Exception e)
+			{
+				System.Windows.MessageBox.Show("Error: " + e.ToString());
+			}
+		}
+	}
+}
